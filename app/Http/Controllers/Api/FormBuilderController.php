7d79286036ebb85225e6fb50a6923a78b5cca4b3
haroonniaz96github <haroonniaz96github@gmail.com>
2023-03-07 19:00:29 +0500

    design changes

--- app/Http/Controllers/Api/FormBuilderController.php
+++ app/Http/Controllers/Api/FormBuilderController.php
@@ -553,7 +553,7 @@ class FormBuilderController extends ApiController
                     $quest = $question->title ?? '';
                     if ($question->type === 'multiple_choice') {
                         $answer = $answer->option->title ?? '';
-                        $point = $answer->option->point ?? '';
+                        $point = $answer->option->point ?? '0';
                         $section_html .= "<tr>
                                 <td style='border: 1px solid lightslategray; padding: 10px; width: 40%; background-color: lightgrey; font-size: 15px'>
                                     <p style='margin-top:8px; margin-bottom:8px; margin-left:8px'>".$quest."</p>
@@ -575,7 +575,7 @@ class FormBuilderController extends ApiController
                                     <p style='margin-top:8px; margin-bottom:8px; margin-left:8px'>".$answer."</p>
                                 </td>
                                 <td style='border: 1px solid lightslategray; padding: 10px'>
-                                    <p style='margin-top:8px; margin-bottom:8px; margin-left:8px'>Hello</p>
+                                    <p style='margin-top:8px; margin-bottom:8px; margin-left:8px'>Nill</p>
                                 </td>
                             </tr>";
                     }
