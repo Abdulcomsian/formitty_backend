c0eea77dae44c52be29532028b3b8389bf8e3bc3
haroonniaz96github <haroonniaz96github@gmail.com>
2023-03-15 18:58:14 +0500

    updated array

--- app/Http/Controllers/Api/AssessmentToolController.php
+++ app/Http/Controllers/Api/AssessmentToolController.php
@@ -40,8 +40,8 @@ class AssessmentToolController extends ApiController
 
         try {
             $assessment_tools = [];
-            $assessment_tools = Response::with('assessment_tool')->where([['user_id', Auth::user()->id ?? '2'], ['user_form_id', $request->user_form_id]])->get();
-            $assessment_tools = FlowchartResponse::with('assessment_tool')->where([['user_id', Auth::user()->id ?? '2'], ['user_form_id', $request->user_form_id]])->get();
+            $assessment_tools[] = Response::with('assessment_tool')->where([['user_id', Auth::user()->id ?? '2'], ['user_form_id', $request->user_form_id]])->get();
+            $assessment_tools[] = FlowchartResponse::with('assessment_tool')->where([['user_id', Auth::user()->id ?? '2'], ['user_form_id', $request->user_form_id]])->get();
             return $this->successResponse($assessment_tools, 'Assessment tools get successfully!.', 200);
         } catch (\Throwable $th) {
             return $this->errorResponse($th->getMessage(), 401);
