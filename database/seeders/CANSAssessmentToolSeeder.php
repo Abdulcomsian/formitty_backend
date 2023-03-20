c5ef8414311fa95c66fc867a6d4a4f34ed0f4b92
haroonniaz96github <haroonniaz96github@gmail.com>
2023-03-20 18:31:31 +0500

    new changes in CANS seeder

--- database/seeders/CANSAssessmentToolSeeder.php
+++ database/seeders/CANSAssessmentToolSeeder.php
@@ -160,7 +160,7 @@ class CANSAssessmentToolSeeder extends Seeder
         $question7 = Question::create([
             'assessment_group_id' => $group1->id,
             'assessment_tool_id' => $assessment_tool->id,
-            'title' => 'continence',
+            'title' => 'Continence',
             'type' => 'multiple_choice',
             'point' => '4.3',
         ]);
@@ -175,7 +175,385 @@ class CANSAssessmentToolSeeder extends Seeder
             'title' => 'No',
         ]);
 
-        for ($i = 2; $i <= 5; $i++) {
+        $question8 = Question::create([
+            'assessment_group_id' => $group1->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Eating and drinking',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question8->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question8->id,
+            'title' => 'No',
+        ]);
+
+        $question9 = Question::create([
+            'assessment_group_id' => $group1->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Transfers/mobility (incl. stairs and indoor surfaces)',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question9->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question9->id,
+            'title' => 'No',
+        ]);
+
+        $question10 = Question::create([
+            'assessment_group_id' => $group1->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Other (specify):',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question10->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question10->id,
+            'title' => 'No',
+        ]);
+
+        $question11 = Question::create([
+            'assessment_group_id' => $group2->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Personal hygiene/toileting',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question11->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question11->id,
+            'title' => 'No',
+        ]);
+
+        $question12 = Question::create([
+            'assessment_group_id' => $group2->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Bathing/dressing ',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question12->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question12->id,
+            'title' => 'No',
+        ]);
+
+        $question13 = Question::create([
+            'assessment_group_id' => $group2->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Preparation of light meal/snack',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question13->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question13->id,
+            'title' => 'No',
+        ]);
+
+        $question14 = Question::create([
+            'assessment_group_id' => $group2->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Other (specify):',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question14->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question14->id,
+            'title' => 'No',
+        ]);
+
+        $question15 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Shopping',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question15->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question15->id,
+            'title' => 'No',
+        ]);
+
+        $question16 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Domestic incl. preparation of main meal',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create([
+            'question_id' => $question16->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question16->id,
+            'title' => 'No',
+        ]);
+
+        $question17 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Medication use',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create($question17
+            'question_id' => $question8->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question17->id,
+            'title' => 'No',
+        ]);
+
+        $question18 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Money management',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question18->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question18->id,
+            'title' => 'No',
+        ]);
+
+        $question19 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Everyday devices (e.g., telephone, television)',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question19->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question19->id,
+            'title' => 'No',
+        ]);
+
+        $question20 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Transport and outdoor surfaces',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question20->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question20->id,
+            'title' => 'No',
+        ]);
+
+        $question21 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Parenting skills',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question21->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question21->id,
+            'title' => 'No',
+        ]);
+
+        $question22 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Interpersonal relationships',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question22->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question22->id,
+            'title' => 'No',
+        ]);
+
+        $question23 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Leisure and recreation',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question23->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question23->id,
+            'title' => 'No',
+        ]);
+
+        $question24 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Employment/study',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question24->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question24->id,
+            'title' => 'No',
+        ]);
+
+        $question25 = Question::create([
+            'assessment_group_id' => $group3->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Other (specify): ',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question25->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question25->id,
+            'title' => 'No',
+        ]);
+
+        $question26 = Question::create([
+            'assessment_group_id' => $group4->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Informational supports (e.g., advice)',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question26->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question26->id,
+            'title' => 'No',
+        ]);
+
+        $question27 = Question::create([
+            'assessment_group_id' => $group4->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Emotional supports ',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question27->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question27->id,
+            'title' => 'No',
+        ]);
+
+        $question28 = Question::create([
+            'assessment_group_id' => $group4->id,
+            'assessment_tool_id' => $assessment_tool->id,
+            'title' => 'Other (specify):',
+            'type' => 'multiple_choice',
+            'point' => '4.3',
+        ]);
+
+        Option::create(
+            'question_id' => $question28->id,
+            'title' => 'Yes',
+        ]);
+
+        Option::create([
+            'question_id' => $question28->id,
+            'title' => 'No',
+        ]);
+
+/*        for ($i = 2; $i <= 5; $i++) {
             for ($j = 1; $j <= 10; $j++) {
 
                 $question = Question::create([
@@ -196,6 +574,6 @@ class CANSAssessmentToolSeeder extends Seeder
                     'title' => 'No',
                 ]);
             }
-        }
+        }*/
     }
 }
