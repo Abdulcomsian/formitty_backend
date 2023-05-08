5996793d22c233d75d70f8d76b8a1bbfeb9b519a
haroonniaz96 <haroonniaz96github@gmail.com>
2023-05-08 18:18:40 +0500

    generating pdf files

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,43 +1,37 @@
 <table style="width: 100%; padding: 1rem">
     <tbody>
         <tr style="margin-top: 1rem">
-            <th style="font-size: x-large">Title</th>
+            <th style="font-size: large">Title</th>
             <td style="font-size: large">{{ $product['data']['name'] }}</td>
         </tr>
         <tr style="margin-top: 1rem">
-            <th style="font-size: x-large">Detail</th>
+            <th style="font-size: large">Detail</th>
             <td style="font-size: large">{{ $product['data']['name'] }}</td>
         </tr>
         <tr style="margin-top: 1rem">
-            <th style="font-size: x-large">Availability</th>
+            <th style="font-size: large">Availability</th>
             <td style="font-size: large">{{ $product['data']['in_stock'] }}</td>
         </tr>
         <tr style="margin-top: ;">
-            <th style="font-size: x-large">Price</th>
+            <th style="font-size: large">Price</th>
             <td style="font-size: large">{{ $product['data']['formated_price'] }}</td>
         </tr>
         <tr>
-            <th style="font-size: x-large">Short Description</th>
-            <td style="font-size: large">{!! $product['data']['short_description'] !!}</td>
+            <th style="font-size: large">Short Description</th>
+            <td style="font-size: large">{{ strip_tags($product['data']['short_description']) }}</td>
         </tr>
         <tr>
-            <th style="font-size: x-large">Description</th>
+            <th style="font-size: large">Description</th>
             <td style="font-size: large">{{ strip_tags($product['data']['description']) }}</td>
         </tr>
         <tr>
-            <th style="font-size: x-large">Image</th>
+            <th style="font-size: large">Images</th>
             <td>
-                @foreach ($product['data']['images'] as $image)
-                    <img src="{{ $image['original_image_url'] }}" alt="{{ $image['id'] }}" width="100px">
-                @endforeach
-            </td>
-        </tr>
-        <tr>
-            <th style="font-size: x-large">Image</th>
-            <td>
-                @foreach ($product['data']['images'] as $image)
-                    <img src="{{ $image['original_image_url'] }}" alt="{{ $image['id'] }}" width="100px">
-                @endforeach
+                <div style="margin-top: 30pt">
+                    @foreach ($product['data']['images'] as $image)
+                        <img src="{{ $image['large_image_url'] }}" alt="{{ $image['id'] }}" height="100pt">
+                    @endforeach
+                </div>
             </td>
         </tr>
     </tbody>
