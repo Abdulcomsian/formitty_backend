de31422b9d66386a71121adea135640abb965fd1
haroonniaz96 <haroonniaz96github@gmail.com>
2023-05-08 11:51:46 +0500

    generate pdf file uploaded

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,16 +1,43 @@
+{{-- @dd($product['data']['name']) --}}
 <table class="table table-bordered mb-5">
     <thead>
         <tr class="table-danger">
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
+            <th scope="col">Title</th>
+            <th scope="col">Detail</th>
         </tr>
     </thead>
     <tbody>
-        @foreach($employee ?? '' as $key=>$value)
         <tr>
-            <th scope="row">{{ $key }}</th>
-            <td>{{ $value }}</td>
+            <th scope="row">Name</th>
+            <td>{{ $product['data']['name']}}</td>
         </tr>
-        @endforeach
+        <tr>
+            <th scope="row">Price</th>
+            <td>{{ $product['data']['formated_price']}}</td>
+        </tr>
+        <tr>
+            <th scope="row">Short Description</th>
+            <td>{!! $product['data']['short_description'] !!}</td>
+        </tr>
+        <tr>
+            <th scope="row">Description</th>
+            <td>{!! $product['data']['description'] !!}</td>
+        </tr>
+        <tr>
+            <th scope="row">Image</th>
+            <td>
+                @foreach($product['data']['images'] as $image)
+                <img src="{{ $image['original_image_url'] }}" alt="{{ $image['id'] }}" width="100px">
+                @endforeach
+            </td>
+        </tr>
+        <tr>
+            <th scope="row">Image</th>
+            <td>
+                @foreach($product['data']['images'] as $image)
+                <img src="{{ $image['original_image_url'] }}" alt="{{ $image['id'] }}" width="100px">
+                @endforeach
+            </td>
+        </tr>
     </tbody>
 </table>
