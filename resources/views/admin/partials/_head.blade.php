d2a034016e30f5149e7a0dc3bc5f9342ceb67fe3
Shahbaz <rajashayzee@yahoo.com>
2023-08-04 13:08:31 +0500

    dashboard design changes

--- resources/views/admin/partials/_head.blade.php
+++ resources/views/admin/partials/_head.blade.php
@@ -1,7 +1,14 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+<!--  <meta name="theme-color" content="#000000" />-->
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/images/apple-touch-icon.png') }}" />
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon-32x32.png') }}" />
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon-16x16.png') }}" />
+<link rel="manifest" href="{{ asset('assets/images/site.webmanifest') }}" />
+
 {!! render_favicon_by_id(get_static_option('site_favicon')) !!}
 <title>Formity | Admin Panel</title>
 
@@ -20,7 +27,6 @@
 <!-- Theme style -->
 <link rel="stylesheet" href="{{ asset('backend/dist/css/adminlte.min.css') }}">
 
-<link rel="icon" type="image/png" sizes="16x16"
-    href="@isset($setting['favicon']){{ asset('uploads/' . $setting['favicon']) }}@endisset">
-    <title>@isset($setting['site_title']){{ $setting['site_title'] }}@endisset</title>
-        @yield('stylesheets')
+<link rel="icon" type="image/png" sizes="16x16" href="@isset($setting['favicon']){{ asset('uploads/' . $setting['favicon']) }}@endisset">
+<title>@isset($setting['site_title']){{ $setting['site_title'] }}@endisset</title>
+@yield('stylesheets')
