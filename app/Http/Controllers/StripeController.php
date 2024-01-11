471c0f941bf39dac480095c3d9e8d1303350118a
nraccrual <nauman.riaz@accrualhub.com>
2024-01-11 14:14:16 +0500

    stripe subscription

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Stripe\Stripe;
 use Stripe\PaymentIntent;
+use Stripe\SetupIntent;
 use App\Models\SubscriptionPlan;
 use Illuminate\Support\Facades\Validator;
 use Stripe\Exception\CardException;
@@ -34,13 +35,17 @@ class StripeController extends Controller
         }
 
         try{
-            $paymentIntent = PaymentIntent::create([
-                'amount' => $subscriptionPlan->amount * 100,
-                'currency' => 'usd',
+            // $paymentIntent = PaymentIntent::create([
+            //     'amount' => $subscriptionPlan->amount * 100,
+            //     'currency' => 'usd',
+            // ]);
+
+            $setupIntent = SetupIntent::create([
                 'payment_method_types' => ['card'],
+                'usage' => 'off_session',
             ]);
 
-            return response()->json(['status' => true , 'paymentIntent' => $paymentIntent->client_secret]);
+            return response()->json(['status' => true , 'setupIntent' => $setupIntent->client_secret]);
 
         }catch(CardException $e){
             return response()->json(['status' => false , 'msg' => 'Something Went Wrong' , 'error' => $e->getMessage()]);
@@ -77,9 +82,9 @@ class StripeController extends Controller
 
            $request->user()->updateDefaultPaymentMethod($request->payment_method);
 
-        //    dd("after updating payment");
+           $paymentMethod = $request->payment_method;
 
-           $subscription = $request->user()->newSubscription('default' , $subscriptionPlan->plan_id)->create();
+           $subscription = $request->user()->newSubscription('default' , $subscriptionPlan->plan_id)->create(null, ['payment_method' => $paymentMethod]);
 
            if($subscription){
                 return response()->json(['status' => true , 'msg' => 'Subscription Added Successfully']);
@@ -91,3 +96,76 @@ class StripeController extends Controller
 
     }
 }
+
+
+// public function addSubscription(Request $request)
+//     {
+//         $validator = Validator::make($request->all() , [
+//             'pid' => 'required|string',
+//             'paymentMethod' =>'required|string'
+//         ]);
+
+//         if($validator->fails()){
+//             return response()->json(['status' => false , 'msg' => 'Something Went Wrong' , 'error' => $validator->getMessageBag()]);
+//         }
+            
+//         try{
+//             $subscriptionPlan = SubscriptionPlan::where('subscription_plan_id' , $request->pid)->first();
+            
+//             if(!$subscriptionPlan){
+//                 return response()->json(['status' => false , 'msg' => 'Something Went Wrong' , 'error' => 'No plan found!']);
+//             }
+
+//             try{
+
+//                 // Stripe::setApiKey(env('STRIPE_SECRET'));
+                
+//                 if(!$request->user()->stripe_id){
+//                     $request->user()->createAsStripeCustomer();
+//                 }
+
+//                 $request->user()->updateDefaultPaymentMethod($request->paymentMethod);
+
+//                 $subscription = $request->user()->newSubscription('default' , $subscriptionPlan->subscription_plan_id)->create($request->paymentMethod);
+
+//                 if($subscription){
+//                     return response()->json(['status' => true , 'msg' => 'Subscription Added Successfully']);
+//                 }
+                
+
+
+//             }catch(CardException $e){
+//                 return response()->json(['status' => false , 'msg' => 'Something Went Wrong']);
+//             }
+
+
+//         }catch(\Exception $e){
+//             return response()->json(['status' => false , 'msg' => 'Something Went Wrong' , 'error' => $e->getMessage()]);
+//         }
+
+
+//     }
+
+//     public function cancelSubscription(){
+//         try{
+//             auth()->user()->subscription('default')->cancel();
+
+//             return redirect()->back()->with(['status' => true , 'msg' => 'Subscription canceled Successfully']);
+
+//         }catch(CardException $e){
+
+//             return redirect()->back()->with(['error' => $e->getMessage() , 'msg' => 'Something Went Wrong While Canceling Subscription' , 'status' => false]);
+//         }
+//     }
+
+//     public function resumeSubscription(){
+//         try{
+//             auth()->user()->subscription('default')->resume();
+
+//             return redirect()->back()->with(['status' => true , 'msg' => 'Subscription Resumed Successfully']);
+
+//         }catch(CardException $e){
+
+//             return redirect()->back()->with(['error' => $e->getMessage() , 'msg' => 'Something Went Wrong While Resuming Subscription' , 'status' => false]);
+//         }
+//     }
